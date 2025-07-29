e1ded510d8784f2c1bb545c0acd66b97cb9d3c35
saurabh <saurbhtilak6@gmail.com>
2025-07-29 09:08:18 +0530

    app.js here all dependiencies import

---
+++ OnlineJobPortal/src/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const router = require("./routes/route.js");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.use("/", router);
+
+
+module.exports = app;
