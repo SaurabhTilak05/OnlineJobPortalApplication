43f4b380a8fba0d8b9a501d07265f569344878ff
SaurabhTilak <saurbhtilak6@gmail.com>
2025-07-29 19:32:42 +0530

    Update code message

--- OnlineJobPortal/src/routes/route.js
+++ OnlineJobPortal/src/routes/route.js
@@ -1,14 +1,27 @@
-const express = require("express");
-const adCtrl = require("../controllers/adminController.js");
-const hrCtrl=require("../controllers/hrcontroller.js");
+let express = require("express");
+let adCtrl = require("../controllers/adminController.js");
+let hrCtrl=require("../controllers/hrcontroller.js");
+let jobctrl=require("../controllers/jobseekerctrl.js");
 
-const router = express.Router();
+let router = express.Router();
 
 // Admin routes
 router.post("/addAdmin", adCtrl.saveAdmin);
-
+router.post("/adminLogin",adCtrl.adminLogin);
 // Hr routes
-router.post("/addHr", hrCtrl.saveHr);
+router.post("/registerHr", hrCtrl.registerHr);
 router.get("/viewHr", hrCtrl.getHrs);
+router.post("/hrLogin",hrCtrl.loginHr);
+
+// update all fields 
+router.put("/UpdateHr",hrCtrl.updateHr);
+
+// Delete hr by Id
+router.delete("/deleteHR",hrCtrl.detHRByID);
+
+
+
+// Job Seeker routes
+router.post("/regJobSeeker",jobctrl.regSeekers);
 
 module.exports = router;
