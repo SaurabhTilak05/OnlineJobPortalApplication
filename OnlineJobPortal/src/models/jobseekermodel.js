ef6ab2989cf485418166e3f45c746b69c8582d87
SaurabhTilak <saurbhtilak6@gmail.com>
2025-07-30 11:24:33 +0530

    Job seekers Operations

--- OnlineJobPortal/src/models/jobseekermodel.js
+++ OnlineJobPortal/src/models/jobseekermodel.js
@@ -15,4 +15,33 @@ exports.regSeeker = (name,  email, password, phone, address) => {
     });
 };
 
+// get all job seekers 
+exports.getAllSeekers = () => {
+    return new Promise((resolve, reject) => {
+        db.query("SELECT * FROM job_seekers", (err, result) => {
+            if (err) {
+                return reject(err);
+            } else {
+                return resolve(result);
+            }
+        });
+    });
+};
 
+// Login job seeker with email and password
+exports.loginSeeker=(email, password)=>{
+    return new Promise((resolve, reject)=>{
+        db.query("select *from job_seekers where email=? and password=?",[email, password],(err, result)=>{
+            if(err || result.length === 0)
+            {
+                console.log(err);
+                
+                return reject("Job seeker Not Login successfull...");
+            }
+            else{
+                  console.log(result);
+                return resolve("Job seeker Login successfull...");
+            }
+        })
+    })   
+}
