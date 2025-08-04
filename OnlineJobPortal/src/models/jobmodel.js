230b96d341992b2b482a1b51c18bd824e777e096
SaurabhTilak <saurbhtilak6@gmail.com>
2025-08-04 15:59:02 +0530

    Job model

--- OnlineJobPortal/src/models/jobmodel.js
+++ OnlineJobPortal/src/models/jobmodel.js
@@ -1,7 +1,7 @@
 const { resolveInclude, promiseImpl } = require("ejs");
 let db=require("../../db.js");
 
-//api for the addjob by the hr 
+
 exports.addJob=(hr_id, title, company, opening, experience_required, location, package, skills_required, description, deadline)=>{
     return new Promise((resolve, reject)=>{
         db.query("INSERT into jobs (hr_id, title, company, opening, experience_required, location, package, skills_required, description, deadline) VALUES (?, ?,?,?,?,?,?,?,?,?)",
@@ -19,8 +19,6 @@ exports.addJob=(hr_id, title, company, opening, experience_required, location, p
 }
 
 
-
-//for the see all job
 exports.getallJobs=()=>{
     return new Promise((resolve, reject)=>{
         db.query("select *from jobs",(err, result)=>{
@@ -35,10 +33,6 @@ exports.getallJobs=()=>{
     })
 }
 
-
-
-//search job by using jobId
-
 exports.getJobById=(job_id)=>{
         return new Promise((resolve, reject)=>{
             db.query("select *from jobs where job_id=?",[job_id],(err,result)=>{
@@ -52,9 +46,6 @@ exports.getJobById=(job_id)=>{
         })
 }
 
-
-
-//update the job 
 exports.updateJob=( title, company, opening, experience_required, location, package, skills_required, description, deadline, job_id)=>{
     return new Promise((resolve, reject)=>{
         db.query("update jobs set title=?, company=?, opening=?, experience_required=?, location=?, package=?, skills_required=?, description=?, deadline=? where job_id=?",
@@ -69,7 +60,6 @@ exports.updateJob=( title, company, opening, experience_required, location, pack
     })
 }
 
-// delete job   
 exports.deleteJob=(job_id)=>{
     return new Promise((resolve, reject)=>{
         db.query("delete from jobs where job_id=?",[job_id],(err, result)=>{
@@ -83,12 +73,9 @@ exports.deleteJob=(job_id)=>{
     })
 }
 
-
-//search job by the title
-
 exports.searchByTitle=(title)=>{
      return new Promise((resolve, reject)=>{
-        db.query("select  *from jobs where title=?",[title],(err, result)=>{
+        db.query("select j.* from jobs j left join hr h on j.hr_id=h.hr_id  where title=?",[title],(err, result)=>{
             if(err){
                 return reject("Something is wrong...");
             }
