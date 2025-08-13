56d93d4ceb1ab912bec017de3d583a5b53ec71d9
SaurabhTilak <saurbhtilak6@gmail.com>
2025-08-13 14:42:38 +0530

    app.js file

--- OnlineJobPortal/src/app.js
+++ OnlineJobPortal/src/app.js
@@ -2,8 +2,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const router = require("./routes/route.js");
 require("dotenv").config();
-
 let app = express();
+// this is cors library
+let cors = require("cors");
+app.use(cors());
+
+
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
