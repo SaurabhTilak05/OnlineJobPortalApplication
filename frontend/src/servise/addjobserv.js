2ac6e6732e12641750770f4786a82ad7be0abaed
SaurabhTilak <saurbhtilak6@gmail.com>
2025-08-13 14:41:39 +0530

    new change

--- frontend/src/servise/addjobserv.js
+++ frontend/src/servise/addjobserv.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 class AddJObService {
    addJob(jobdata) {
-  return axios.post("http://localhost:8080/AddJob", jobdata ,{
-    headers: {
-      'Content-Type': 'application/json'
-         }
+      return axios.post("http://localhost:8080/AddJob", jobdata ,{
         });
     } 
 }
