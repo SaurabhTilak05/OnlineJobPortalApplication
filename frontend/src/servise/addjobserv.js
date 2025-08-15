2105ce9b93d55c0efde81504802958bc4ae4b2f2
SaurabhTilak <saurbhtilak6@gmail.com>
2025-08-16 00:07:54 +0530

    servise layer

--- frontend/src/servise/addjobserv.js
+++ frontend/src/servise/addjobserv.js
@@ -4,5 +4,9 @@ class AddJObService {
       return axios.post("http://localhost:8080/AddJob", jobdata ,{
         });
     } 
+    contactUs(cont){
+      return axios.post("http://localhost:8080/contact",cont ,{
+      });
+    }
 }
 export default new AddJObService();
