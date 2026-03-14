c7346dcfd79165644f2c9ceb4df6d57e79d3291c
SaurabhTilak <saurbhtilak6@gmail.com>
2026-03-14 18:22:01 +0530

    new change in file

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const router = require("./routes/route.js");
 const path = require("path"); 
+const multer = require("multer");
 
 require("dotenv").config();
 let app = express();
@@ -21,5 +22,22 @@ app.use("/resumes", express.static(path.join(__dirname, "../public/resumes")));
 app.set("view engine", "ejs");
 app.use("/", router);
 
+app.use((err, req, res, next) => {
+  if (err instanceof multer.MulterError) {
+    if (err.code === "LIMIT_FILE_SIZE") {
+      return res.status(400).json({ message: "File size must be 5MB or less" });
+    }
+
+    return res.status(400).json({ message: err.message });
+  }
+
+  if (err) {
+    return res.status(err.statusCode || 500).json({
+      message: err.message || "Internal server error",
+    });
+  }
+
+  next();
+});
 
 module.exports = app;
