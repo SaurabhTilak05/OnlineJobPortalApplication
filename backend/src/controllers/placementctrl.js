f58d1a451492e9e0252f072f1fd2d1fbca82217b
SaurabhTilak <saurbhtilak6@gmail.com>
2026-03-14 18:24:14 +0530

    new change

--- backend/src/controllers/placementctrl.js
+++ backend/src/controllers/placementctrl.js
@@ -2,26 +2,39 @@ const db = require("../../db.js");
 
 exports.getPlacements = async (req, res) => {
   try {
-    const hrId = req.user?.id || req.query.hrId;
+    const isHrRequest = req.user?.role === "hr";
 
-    const baseQuery = `
+    const publicQuery = `
+      SELECT 
+        p.id AS placement_id,
+        p.placement_date,
+        p.status AS placement_status,
+        s.name AS seeker_name,
+        sp.profile_picture,
+        j.title AS job_title,
+        j.company
+      FROM placement p
+      INNER JOIN job_seekers s ON p.seeker_id = s.seeker_id
+      LEFT JOIN student_profiles sp ON s.seeker_id = sp.seeker_id
+      INNER JOIN jobs j ON p.job_id = j.job_id
+      ORDER BY p.placement_date DESC
+    `;
+
+    const hrQuery = `
       SELECT 
         p.id AS placement_id,
         p.placement_date,
         p.status AS placement_status,
         p.remarks,
-
         s.seeker_id,
         s.name AS seeker_name,
         s.email AS seeker_email,
         sp.profile_picture,
-
         sp.college_name,
         sp.branch,
         sp.graduation_year,
         sp.percentage,
         sp.skills,
-
         j.job_id,
         j.title AS job_title,
         j.company,
@@ -31,15 +44,13 @@ exports.getPlacements = async (req, res) => {
       INNER JOIN job_seekers s ON p.seeker_id = s.seeker_id
       LEFT JOIN student_profiles sp ON s.seeker_id = sp.seeker_id
       INNER JOIN jobs j ON p.job_id = j.job_id
+      WHERE j.hr_id = ?
+      ORDER BY p.placement_date DESC
     `;
 
-    const query = hrId
-      ? `${baseQuery} WHERE j.hr_id = ? ORDER BY p.placement_date DESC`
-      : `${baseQuery} ORDER BY p.placement_date DESC`;
-
-    const [rows] = hrId
-      ? await db.query(query, [hrId])
-      : await db.query(query);
+    const [rows] = isHrRequest
+      ? await db.query(hrQuery, [req.user.id])
+      : await db.query(publicQuery);
 
     res.status(200).json(rows);
   } catch (err) {
